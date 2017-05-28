4b7538875cbcdc5857ba18652e4c32a9b0782b22
Christopher Lawes <wahnbriefe89@gmail.com>
2017-05-28 21:36:58 +0100

    add axios to main dependencies, and assign to global window

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@
 // so you don't have to do: import Vue from 'vue/dist/vue'
 // This is done with the browser options. For the config, see package.json
 import Vue from 'vue'
+import axios from 'axios'
+
 import App from './App.vue'
+
+window.axios = axios;
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
